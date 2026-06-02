28e5f8bb6a94ff55faf2a137d79170b2ebce0900
Warre Neirinck <warre.neirinck@gmail.com>
2026-06-02 12:29:37 +0200

    feat: add TextInput component to RoleForm and make 'name' field required; enhance RolesTable with searchable 'name' column

--- app/Filament/Resources/Roles/Tables/RolesTable.php
+++ app/Filament/Resources/Roles/Tables/RolesTable.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Roles\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 class RolesTable
@@ -13,7 +14,8 @@ class RolesTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name')
+                    ->searchable(),
             ])
             ->filters([
                 //
